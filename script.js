637748631b2505b59a28d95803b9b235a3052668
Alex Francisco <alex2007f@gmail.com>
2020-11-11 11:18:43 -0300

    praticando...

---
+++ script.js
@@ -0,0 +1,6 @@
+function verificar() {
+    var data = new Date()
+    var anoAtual = data.getFullYear()
+    var anoNasc = document.getElementById('ano_nasc')
+    var resultado = document.getElementById('resultado')  
+}
